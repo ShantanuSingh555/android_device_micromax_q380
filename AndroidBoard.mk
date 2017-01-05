47d38d938c887f0d69f44fbb12f7c7212359beec
ShankarWarang <shankar.warang9@gmail.com>
2017-01-06 00:15:22 +0530

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/micromax/q380/Android.mk
+-include vendor/micromax/q380/q380-vendor.mk
